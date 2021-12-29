7266ddf1f4df70bff480074e018c2077767ca871
Gabriele-Frattini <96744665+Gabriele-Frattini@users.noreply.github.com>
2021-12-29 13:07:11 +0100

    removed age and height from app

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from pathlib import Path
 load_model = pickle.load(open('model.pkl', 'rb'))
 
 
-def predict_fight(draws, losses, wins, opponent_wins, opponent_losses, opponent_draws, Height_Difference, Age_Difference):
-    input = np.array([[draws, losses, wins, opponent_wins,
-                     opponent_losses, opponent_draws, Height_Difference, Age_Difference]]).astype(np.float64)
+def predict_fight(wins, losses, draws, opponent_wins, opponent_losses, opponent_draws):
+    input = np.array([[wins, losses, draws, opponent_wins,
+                     opponent_losses, opponent_draws]]).astype(np.float64)
     prediction = load_model.predict(input)
     return prediction
 
@@ -16,20 +16,16 @@ def predict_fight(draws, losses, wins, opponent_wins, opponent_losses, opponent_
 def main():
     st.title("Predict the result for our fighter")
 
-    wins = st.slider("Wins", 1, 70,10)
-    losses = st.slider("Losses", 1, 70,10)
-    draws = st.slider("Draws", 1, 70,10)
-    age = st.slider("Age",17,55,25)
-    height = st.slider("Height",150,220,175)
-    opponent_wins = st.slider("Opponent wins", 1, 70,10)
-    opponent_losses = st.slider("Opponent losses", 1, 70,10)
-    opponent_draws = st.slider("Opponent draws", 1, 70,10)
-    age_opponent = st.slider("Opponent age",17,55,15)
-    height_opponent = st.slider("Opponent height",150,220,175)
-    ok = st.button("Predict result")
+    wins = st.slider("wins", 1, 70)
+    losses = st.slider("losses", 1, 70)
+    draws = st.slider("draws", 1, 70)
+    opponent_wins = st.slider("opponent wins", 1, 70)
+    opponent_losses = st.slider("opponent losses", 1, 70)
+    opponent_draws = st.slider("opponent draws", 1, 70)
+    ok = st.button("predict")
     if ok:
-        output = predict_fight(draws, losses, wins,
-                               opponent_wins, opponent_losses, opponent_draws, (height-height_opponent),(age-age_opponent))
+        output = predict_fight(wins, losses, draws,
+                               opponent_wins, opponent_losses, opponent_draws)
         st.subheader(
             f"The predicted result for our fighter is a {output[0].lower()}")
 
