5dbe80936d7d7ee74bf568555fa38b4626c3c3c9
Gabriele-Frattini <96744665+Gabriele-Frattini@users.noreply.github.com>
2021-12-30 12:44:07 +0100

    fixed bug when deploying to streamlit

--- app.py
+++ app.py
@@ -8,15 +8,9 @@ img = Image.open("boxing_winner.jpg")
 load_model = pickle.load(open('model.pkl', 'rb'))
 
 
-<<<<<<< HEAD
 def predict_fight(draws, losses, wins, opponent_wins, opponent_losses, opponent_draws, Height_Difference):
     input = np.array([[draws, losses, wins, opponent_wins,
                      opponent_losses, opponent_draws, Height_Difference]]).astype(np.float64)
-=======
-def predict_fight(wins, losses, draws, opponent_wins, opponent_losses, opponent_draws):
-    input = np.array([[wins, losses, draws, opponent_wins,
-                     opponent_losses, opponent_draws]]).astype(np.float64)
->>>>>>> a6698e570f061bbbe5d2a5f072ea6df8e1319949
     prediction = load_model.predict(input)
     return prediction
 
@@ -24,7 +18,6 @@ def predict_fight(wins, losses, draws, opponent_wins, opponent_losses, opponent_
 def main():
     st.title("Predict the result for our fighter")
 
-<<<<<<< HEAD
     wins = st.slider("Wins", 1, 70,10)
     losses = st.slider("Losses", 1, 70,10)
     draws = st.slider("Draws", 1, 70,10)
@@ -37,18 +30,6 @@ def main():
     if ok:
         output = predict_fight(draws, losses, wins,
                                opponent_wins, opponent_losses, opponent_draws, (height-height_opponent))
-=======
-    wins = st.slider("wins", 1, 70)
-    losses = st.slider("losses", 1, 70)
-    draws = st.slider("draws", 1, 70)
-    opponent_wins = st.slider("opponent wins", 1, 70)
-    opponent_losses = st.slider("opponent losses", 1, 70)
-    opponent_draws = st.slider("opponent draws", 1, 70)
-    ok = st.button("predict")
-    if ok:
-        output = predict_fight(wins, losses, draws,
-                               opponent_wins, opponent_losses, opponent_draws)
->>>>>>> a6698e570f061bbbe5d2a5f072ea6df8e1319949
         st.subheader(
             f"The predicted result for our fighter is a {output[0].lower()}")
 
